cc3b08949bb5504ca4feae5590bcb4f4565516ca
syamaladevi0902 <syamaladevi0902@gmail.com>
2025-10-28 19:27:17 +0530

    Add basic arithmetic functions and main execution

---
+++ my_project/app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Return the difference of two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Add 5 + 3 =", add(5, 3))
+    print("Subtract 5 - 3 =", subtract(5, 3))
